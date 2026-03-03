6ed9e0608d7a1f7936d653b8833b1cca2658a542
M Aslam <aslam4webz@gmail.com>
2026-03-04 02:38:14 +0530

    style: configure Filament admin panel with new favicon

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -28,6 +28,7 @@ class AdminPanelProvider extends PanelProvider
             ->id('admin')
             ->path('admin')
             ->login()
+            ->favicon(asset('favicon.png'))
             ->spa()
             ->colors([
                 'primary' => Color::Amber,
